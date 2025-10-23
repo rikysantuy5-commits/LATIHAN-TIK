0e461b88029d7558beed4b96093bc2cc2f86c82e
rikysantuy5-commits <rikysantuy5@gmail.com>
2025-10-23 11:08:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const tombolsapa = document.getElementById('sapabutton');
 // 2. tambahkan 'event listener' untuk aksi 'click'
 tombolsapa.addEventListener('click',function() {
   // 3. jalankan kode ini ketika tombol di klik
- alert('hallo!terima kasih sudah berkunjung!');
+ alert('hallo! terima kasih sudah berkunjung!');
 });
