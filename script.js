fc2172ea471c3a9809a1497f1bd4224dd8d09219
rikysantuy5-commits <rikysantuy5@gmail.com>
2025-10-23 11:17:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // 1. pilih tombol berdasarkan id-nya
-const tombolsapa = document.getElementById('sapabutton');
+const tombolsapa = document.getElementById('sapa button');
 // 2. tambahkan 'event listener' untuk aksi 'click'
 tombolsapa.addEventListener('click',function() {
   // 3. jalankan kode ini ketika tombol di klik
